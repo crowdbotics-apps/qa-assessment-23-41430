bf224ae031e625a1a1a0c3f7ce8352fdba193ee6
Crowdbotics <team@crowdbotics.com>
2023-06-12 11:11:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "qa-assessment-23-41430",
+  url: "https://qa-assessment-23-41430.botics.co",
+  api: "https://qa-assessment-23-41430.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
